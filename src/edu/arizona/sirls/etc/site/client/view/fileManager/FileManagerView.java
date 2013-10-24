2faf7b0025a84c26beaac42eb63a08e4ad6702f0
rodenhausen <thomas.rodenhausen@gmail.com>
2013-10-24 15:13:40 -0700

    added consistent sub-site heading

--- src/edu/arizona/sirls/etc/site/client/view/fileManager/FileManagerView.java
+++ src/edu/arizona/sirls/etc/site/client/view/fileManager/FileManagerView.java
@@ -3,6 +3,7 @@ package edu.arizona.sirls.etc.site.client.view.fileManager;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -19,6 +20,9 @@ public class FileManagerView extends Composite implements FileManagerPresenter.D
 		this.htmlPanel = new HTMLPanel("<div class='content900pxCentered'>" +
 				"<div id='fileManagerContent'></div></div>");
 		VerticalPanel verticalPanel = new VerticalPanel();
+		Label headingLabel = new Label("File Manager");
+		headingLabel.addStyleName("siteHeading");
+		verticalPanel.add(headingLabel);
 		fileManagerPanel = new SimplePanel();
 		verticalPanel.add(fileManagerPanel);
 		markupReviewAnchor = new Anchor("Review Markup");
--- src/edu/arizona/sirls/etc/site/client/view/matrixGeneration/MatrixGenerationView.java
+++ src/edu/arizona/sirls/etc/site/client/view/matrixGeneration/MatrixGenerationView.java
@@ -17,8 +17,12 @@ public abstract class MatrixGenerationView extends Composite {
 				"<div id='matrixGenerationContent'></div></div>");
 		
 		VerticalPanel verticalPanel = new VerticalPanel();
+		Label headingLabel = new Label("Matrix Generation");
+		headingLabel.addStyleName("siteHeading");
+		verticalPanel.add(headingLabel);
 		HorizontalPanel horizontalPanel = new HorizontalPanel();
 		verticalPanel.add(horizontalPanel);		
+		horizontalPanel.addStyleName("submenu");
 		int i=0;
 		for(TaskStageEnum step : TaskStageEnum.values()) {
 			ImageLabelComposite stepEntry = new ImageLabelComposite("images/Enumeration_unselected_" + i++ + ".gif", "20px", "20px", step.displayName());
