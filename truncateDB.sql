e7f2f46ff3d5666b1a1f72a317adfcd198da7495
rodenhausen <thomas.rodenhausen@gmail.com>
2013-11-20 11:05:18 -0700

    updated truncateDB.sql

--- truncateDB.sql
+++ truncateDB.sql
@@ -7,12 +7,49 @@
 -- taskstages
 --
 
+delimiter //
+create procedure drop_charaparser_table(IN pattern VARCHAR(100)) 
+begin
+	SELECT CONCAT('DROP TABLE IF EXISTS ',GROUP_CONCAT(CONCAT(database(),'.',table_name)),';')
+	INTO @droplike
+	FROM information_schema.tables
+	WHERE table_schema = database()
+	AND table_name LIKE pattern;
+    if @droplike is not null then 
+		PREPARE stmt FROM @droplike;
+		EXECUTE stmt;
+		DEALLOCATE PREPARE stmt;
+    end if;
+end;
+//
+delimiter ;
+call drop_charaparser_table('%_allwords');
+call drop_charaparser_table('%_discounted');
+call drop_charaparser_table('%_heuristicnouns');
+call drop_charaparser_table('%_isa');
+call drop_charaparser_table('%_modifiers');
+call drop_charaparser_table('%_permanentglossary');
+call drop_charaparser_table('%_sentence');
+call drop_charaparser_table('%_sentinfile');
+call drop_charaparser_table('%_singularplural');
+call drop_charaparser_table('%_substructure');
+call drop_charaparser_table('%_syns');
+call drop_charaparser_table('%_term_category');
+call drop_charaparser_table('%_unknownwords');
+call drop_charaparser_table('%_wordpos');
+call drop_charaparser_table('%_wordroles');
+
+drop procedure drop_charaparser_table;
+
+DROP TABLE IF EXISTS `datasetprefixes`;
+
+SET foreign_key_checks = 0;
+
 -- truncate shares
 TRUNCATE TABLE  `shareinvitees`;
 
-
 -- truncate files in use
-TRUNCATE TABLE  `taskfiles`;
+TRUNCATE TABLE  `tasksfiles`;
 TRUNCATE TABLE  `filesinuse`;
 
 -- truncate task configurations
@@ -28,3 +65,5 @@ TRUNCATE TABLE  `configurations`;
 -- truncate tasks
 TRUNCATE TABLE  `shares`;
 TRUNCATE TABLE  `tasks`;
+
+SET foreign_key_checks = 1;
