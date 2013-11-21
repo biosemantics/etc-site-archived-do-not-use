4018c3450f9485eb8d12cbed74221bb0ee9c926e
rodenhausen <thomas.rodenhausen@gmail.com>
2013-11-21 11:51:14 -0700

    refactored task and task stage; one task stage type per task type

---
+++ src/edu/arizona/sirls/etc/site/shared/rpc/matrixGeneration/TaskStageEnum.java
@@ -0,0 +1,18 @@
+package edu.arizona.sirls.etc.site.shared.rpc.matrixGeneration;
+
+public enum TaskStageEnum {
+	
+    INPUT("Input"),
+    PROCESS("Process"),
+    OUTPUT("Output");
+
+    private String displayName;
+
+    private TaskStageEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String displayName() { 
+    	return displayName; 
+    }
+}
