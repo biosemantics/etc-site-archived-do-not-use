54070ef87a28aad7aa014e45a833dec78379fa2b
rodenhausen <thomas.rodenhausen@gmail.com>
2014-02-25 15:24:32 -0700

    introdcued centered content panel -> not all content has to be centered and statically layout for optimized width 950px

---
+++ src/main/java/edu/arizona/biosemantics/etcsite/client/layout/CenteredContentPanel.java
@@ -0,0 +1,71 @@
+package edu.arizona.biosemantics.etcsite.client.layout;
+
+import java.util.Iterator;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.AcceptsOneWidget;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasOneWidget;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.IsWidget;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+public class CenteredContentPanel extends Composite implements AcceptsOneWidget, HasWidgets, HasWidgets.ForIsWidget, java.lang.Iterable<Widget> {
+
+	private static CenteredPanelUiBinder uiBinder = GWT
+			.create(CenteredPanelUiBinder.class);
+
+	interface CenteredPanelUiBinder extends UiBinder<Widget, CenteredContentPanel> {
+	}
+
+	@UiField
+	SimplePanel centeredContentPanel;
+	
+	public CenteredContentPanel() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@Override
+	public void add(IsWidget w) {
+		centeredContentPanel.add(w);
+	}
+
+	@Override
+	public boolean remove(IsWidget w) {
+		return centeredContentPanel.remove(w);
+	}
+
+	@Override
+	public void add(Widget w) {
+		centeredContentPanel.add(w);
+	}
+
+	@Override
+	public void clear() {
+		centeredContentPanel.clear();
+	}
+
+	@Override
+	public Iterator<Widget> iterator() {
+		return centeredContentPanel.iterator();
+	}
+
+	@Override
+	public boolean remove(Widget w) {
+		return centeredContentPanel.remove(w);
+	}
+
+	@Override
+	public void setWidget(IsWidget w) {
+		centeredContentPanel.setWidget(w);
+	}
+
+}
