a40b30e6ef335001595ceedf27e735dd16543ad9
rodenhausen <thomas.rodenhausen@gmail.com>
2016-02-02 12:45:54 -0700

    missing FileSource

---
+++ filemanager/src/main/java/edu/arizona/biosemantics/etcsite/filemanager/shared/model/FileSource.java
@@ -0,0 +1,9 @@
+package edu.arizona.biosemantics.etcsite.filemanager.shared.model;
+
+import java.io.Serializable;
+
+public enum FileSource implements Serializable {
+
+	SYSTEM, OWNED, SHARED, PUBLIC;
+	
+}
