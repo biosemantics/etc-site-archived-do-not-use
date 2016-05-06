c81c75bf68db46a597a0c2691280c268afca0f7b
rodenhausen <thomas.rodenhausen@gmail.com>
2016-05-06 13:45:03 -0700

    corrected error

--- src/main/java/edu/arizona/biosemantics/etcsite/client/content/semanticMarkup/SemanticMarkupReviewPresenter.java
+++ src/main/java/edu/arizona/biosemantics/etcsite/client/content/semanticMarkup/SemanticMarkupReviewPresenter.java
@@ -292,7 +292,7 @@ public class SemanticMarkupReviewPresenter implements ISemanticMarkupReviewView.
 		userService.hasLinkedOTOAccount(Authentication.getInstance().getToken(), new AsyncCallback<Boolean>() {
 			@Override
 			public void onFailure(Throwable caught) {
-				Alerter.failedToReview(caught);
+				Alerter.showAlert("Review terms2", "Failed to load terms for review");
 				Alerter.stopLoading(box);
 			}
 			@Override
@@ -324,7 +324,7 @@ public class SemanticMarkupReviewPresenter implements ISemanticMarkupReviewView.
 					}
 					@Override
 					public void onFailure(Throwable caught) {
-						Alerter.failedToReview(caught);
+						Alerter.showAlert("Review terms", "Failed to load terms for review");
 						Alerter.stopLoading(box);
 					}
 				});
