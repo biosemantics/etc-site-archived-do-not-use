6aa61f9cd73bc619e0e8a8f044ac01e551b05984
hongcui <hong1.cui@gmail.com>
2017-01-20 16:41:41 -0700

    setup

--- src/main/java/edu/arizona/biosemantics/etcsite/server/rpc/semanticmarkup/SemanticMarkupService.java
+++ src/main/java/edu/arizona/biosemantics/etcsite/server/rpc/semanticmarkup/SemanticMarkupService.java
@@ -251,8 +251,8 @@ public class SemanticMarkupService extends RemoteServiceServlet implements ISema
 			}
 			String bioportalUserId = daoManager.getUserDAO().getUser(authenticationToken.getUserId()).getBioportalUserId();
 			String bioportalAPIKey = daoManager.getUserDAO().getUser(authenticationToken.getUserId()).getBioportalAPIKey();
-			final Learn learn = new ExtraJvmLearn(daoManager, taxonGroup, useEmptyGlossary, input, tablePrefix, source, operator);
-			//final Learn learn = new InJvmLearn(daoManager, fileService, taxonGroup, useEmptyGlossary, input, tablePrefix, source, operator);
+			//final Learn learn = new ExtraJvmLearn(daoManager, taxonGroup, useEmptyGlossary, input, tablePrefix, source, operator);
+			final Learn learn = new InJvmLearn(daoManager, fileService, taxonGroup, useEmptyGlossary, input, tablePrefix, source, operator);
 			activeLearns.put(config.getConfiguration().getId(), learn);
 			final ListenableFuture<LearnResult> futureResult = executorService.submit(learn);
 			try {
